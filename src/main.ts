950b3274334d2b5a65108ee01a313736b903e259
KhrapkoVasyl <khrapko2002@gmail.com>
2023-03-27 12:24:18 +0300

    chore: add using env variables to set up server

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,20 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get<ConfigService>(ConfigService);
 
-  const HOST = 'localhost';
-  const PORT = 8080;
+  const HOST = configService.get('NEST_HOST');
+  const PORT = configService.get('NEST_PORT');
+  const GLOBAL_PREFIX = configService.get('GLOBAL_PREFIX');
+
+  app.setGlobalPrefix(GLOBAL_PREFIX);
 
   await app.listen(PORT, HOST, () => {
     console.log(`Server listens on http://${HOST}:${PORT}`);
   });
 }
+
 bootstrap();
