6d17ada3c168054a069f068ed4c1670ce7d65bbe
KhrapkoVasyl <khrapko2002@gmail.com>
2023-03-28 18:37:40 +0300

    fix(database): fix converting of boolean strings in config

--- src/systems/database/database.module.ts
+++ src/systems/database/database.module.ts
@@ -2,6 +2,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { join } from 'path';
+import { convertBoolStrToBoolean } from '../../common/helpers';
 
 @Module({
   imports: [
@@ -11,13 +12,13 @@ import { join } from 'path';
         name: configService.get('TYPEORM_NAME'),
         host: configService.get('TYPEORM_HOST'),
         port: configService.get('TYPEORM_PORT'),
-        cache: configService.get('TYPEORM_CACHE'),
+        cache: convertBoolStrToBoolean(configService.get('TYPEORM_CACHE')),
         logging: configService.get('TYPEORM_LOGGING'),
         database: configService.get<string>('TYPEORM_DATABASE'),
         username: configService.get('TYPEORM_USERNAME'),
         password: configService.get('TYPEORM_PASSWORD'),
         extra: {
-          ssl: configService.get('TYPEORM_SSL'),
+          ssl: convertBoolStrToBoolean(configService.get('TYPEORM_SSL')),
         },
         dropSchema: configService.get('TYPEORM_DROP_SCHEMA'),
         synchronize: configService.get('TYPEORM_SYNCHRONIZE'),
