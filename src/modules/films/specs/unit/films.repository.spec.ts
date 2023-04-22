ecc3561c208505db7c2261c04030c1037e53004f
KhrapkoVasyl <khrapko2002@gmail.com>
2023-04-22 16:44:44 +0300

    test(films): update unit tests for repository

--- src/modules/films/specs/unit/films.repository.spec.ts
+++ src/modules/films/specs/unit/films.repository.spec.ts
@@ -1,13 +1,19 @@
-import { plainToInstance } from 'class-transformer';
 import { Test, TestingModule } from '@nestjs/testing';
 import { FilmEntity } from '../../entities';
 import { FilmsRepository } from '../../films.repository';
 import { getRepositoryToken } from '@nestjs/typeorm';
+import { FilmGenreEnum } from '../../enums';
 
 describe('FilmsRepository unit tests', () => {
   let repository: FilmsRepository;
 
-  const filmEntity = new FilmEntity();
+  const filmEntity = {
+    id: '7e8c4c29-cd12-44b7-84aa-02f3e721f706',
+    name: 'films repository test entity',
+    genre: FilmGenreEnum.ACTION,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  } as FilmEntity;
 
   beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -17,21 +23,11 @@ describe('FilmsRepository unit tests', () => {
         {
           provide: getRepositoryToken(FilmEntity),
           useValue: {
-            create: (entity: Partial<FilmEntity>) =>
-              plainToInstance(FilmEntity, { ...entity, ...filmEntity }),
-            save: async (entity: Partial<FilmEntity>) =>
-              plainToInstance(FilmEntity, { ...entity, ...filmEntity }),
+            create: () => filmEntity,
+            save: async () => filmEntity,
             find: async () => [filmEntity],
             findOne: async () => filmEntity,
-            merge: (
-              entityToUpdate: Partial<FilmEntity>,
-              entity: Partial<FilmEntity>,
-            ) =>
-              plainToInstance(FilmEntity, {
-                ...entityToUpdate,
-                ...filmEntity,
-                ...entity,
-              }),
+            merge: () => filmEntity,
             remove: async () => new FilmEntity(),
           },
         },
@@ -48,40 +44,42 @@ describe('FilmsRepository unit tests', () => {
   describe('createOne', () => {
     it('should return created FilmEntity', async () => {
       const received = await repository.createOne(filmEntity);
-      expect(received).toBeInstanceOf(FilmEntity);
+
+      expect(received).toEqual(filmEntity);
     });
   });
 
   describe('findAll', () => {
     it('should return array of found FilmEntities', async () => {
+      const expected = [filmEntity];
+
       const received = await repository.findAll();
+
       expect(Array.isArray(received)).toBe(true);
-      for (const entity of received) {
-        expect(entity).toBeInstanceOf(FilmEntity);
-      }
+      expect(received).toEqual(expected);
     });
   });
 
   describe('findOne', () => {
     it('should return found FilmEntity', async () => {
-      const received = await repository.findOne({ ...filmEntity });
-      expect(received).toBeInstanceOf(FilmEntity);
+      const received = await repository.findOne(filmEntity);
+
+      expect(received).toEqual(filmEntity);
     });
   });
 
   describe('updateOne', () => {
     it('should return updated FilmEntity', async () => {
-      const received = await repository.updateOne(
-        { ...filmEntity },
-        { name: 'new name' },
-      );
-      expect(received).toBeInstanceOf(FilmEntity);
+      const received = await repository.updateOne(filmEntity, {});
+
+      expect(received).toBe(filmEntity);
     });
   });
 
   describe('removeOne', () => {
     it('should return removed FilmEntity', async () => {
-      const received = await repository.removeOne({ ...filmEntity });
+      const received = await repository.removeOne(filmEntity);
+
       expect(received).toBeInstanceOf(FilmEntity);
     });
   });
